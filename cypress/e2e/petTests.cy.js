453853d0e4995e08bf53600abaa29dd5d980341f
Digitalisss <95092934+Digitalisss@users.noreply.github.com>
2023-06-08 00:01:04 +0300

    add petTests and Homework

---
+++ cypress/e2e/petTests.cy.js
@@ -0,0 +1,129 @@
+import pet from '../fixtures/pet.json'
+import { faker } from '@faker-js/faker';
+
+pet.id = faker.number.int()
+pet.name = faker.animal.cat.name
+pet.category.id = faker.number.int(3)
+pet.category.name = faker.animal.type()
+
+// it('Create pet', () => {
+//     cy.log(`Create pet with id: ${pet.id}`)
+
+//     cy.request('POST', '/pet', pet).then( response => {
+//       expect(response.status).to.be.equal(200);
+//       expect(response.body.id).to.be.equal(pet.id);
+//       expect(response.body.name).to.be.equal(pet.name);
+//     })
+// })
+
+it('Create pet', () => {
+  cy.log(`Create pet with id: ${pet.id}`)
+
+  cy.request({
+    method: 'POST',
+    url: '/pet',
+    body: pet,
+    failOnStatusCode: false
+  }).then( response => {
+    expect(response.status).to.be.equal(200);
+    expect(response.body.id).to.be.equal(pet.id);
+    expect(response.body.name).to.be.equal(pet.name);
+  })
+})
+
+it('Get pet by id', () => {
+  cy.log(`Get pet with id: ${pet.id}`)
+
+  cy.request('GET', `/pet/${pet.id}`).then( response => {
+    expect(response.status).to.be.equal(200);
+    expect(response.body.id).to.be.equal(pet.id);
+    expect(response.body.name).to.be.equal(pet.name);
+    expect(response.body.category.id).to.be.equal(pet.category.id);
+    expect(response.body.category.name).to.be.equal(pet.category.name);
+  })
+})
+
+it('Update pet', () => {
+  cy.log(`Update pet with id: ${pet.id}`)
+
+  pet.name = 'Qweqwe';
+  pet.status = 'sold'
+  cy.request('PUT', '/pet', pet).then( response => {
+    expect(response.status).to.be.equal(200);
+    expect(response.body.id).to.be.equal(pet.id);
+    expect(response.body.name).to.be.equal(pet.name);
+    expect(response.body.status).to.be.equal(pet.status);
+  })
+
+  cy.request('GET', `/pet/${pet.id}`).then( response => {
+    expect(response.status).to.be.equal(200);
+    expect(response.body.id).to.be.equal(pet.id);
+    expect(response.body.name).to.be.equal(pet.name);
+    expect(response.body.category.id).to.be.equal(pet.category.id);
+    expect(response.body.category.name).to.be.equal(pet.category.name);
+  })
+})
+
+it('Find pet by status', () => {
+  cy.log(`Find pet with id: ${pet.id}`)
+
+  cy.request('GET', `/pet/findByStatus?status=${pet.status}`).then( response => {
+    expect(response.status).to.be.equal(200);
+
+    let pets = response.body;
+    let resultPetArray = pets.filter( myPet => {
+      return myPet.id === pet.id
+    })
+
+    expect(resultPetArray[0]).to.be.eql(pet);
+  })
+})
+
+
+it('Update pet with form data', () => {
+
+  
+  const updatedStatus = 'sold';
+  const updatedName = 'newName';
+  
+
+  cy.request({
+    method: 'POST',
+    url: `/pet/${pet.id}`,
+    form: true,
+    body: {
+      name: updatedName,
+      status: updatedStatus
+    },
+    failOnStatusCode: false,
+  }).then( response => {
+    expect(response.status).to.be.equal(200);
+  });
+  cy.request('GET', `/pet/${pet.id}`).then( response => {
+    expect(response.status).to.be.equal(200);
+    expect(response.body.id).to.be.equal(pet.id);
+    expect(response.body.name).to.be.equal(updatedName);
+    expect(response.body.status).to.eq(updatedStatus);
+  })
+});
+
+it('Delete pet', () => {
+  cy.log(`Delete pet with id ${pet.id}`)
+
+  cy.request({
+    method: 'DELETE',
+    url: `/pet/${pet.id}`,
+    body: pet,
+    failOnStatusCode: false
+  }).then(response => {
+    expect(response.status).to.be.equal(200);
+  });
+
+  cy.request({
+    method: 'GET',
+    url: `/pet/${pet.id}`,
+    failOnStatusCode: false
+  }).then(response => {
+    expect(response.status).to.be.equal(404);
+  });
+});
